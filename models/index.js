a3ed313e778123e23fbe041453a3a5b5e195b6a9
Roman Stigler <fromanx@gmail.com>
2023-02-17 13:52:18 -0600

    updated models, defined relationships

--- models/index.js
+++ models/index.js
@@ -5,13 +5,22 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'product_id'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  foreignKey: 'product_id',
+  onDelete: 'CASCADE',
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  foreignKey: 'product_id'
+});
 module.exports = {
   Product,
   Category,
