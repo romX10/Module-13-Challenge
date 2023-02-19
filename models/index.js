dead59449dffebb1b3fcac61fa4fc0e2a82dbe9e
Roman Stigler <fromanx@gmail.com>
2023-02-19 13:46:38 -0600

    added functionality to all routes

--- models/index.js
+++ models/index.js
@@ -6,20 +6,27 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'product_id'
+  foreignKey: 'category_id'
 });
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'product_id',
-  onDelete: 'CASCADE',
+  foreignKey: 'category_id'
 });
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  foreignKey: 'product_id'
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'product_tags'
 });
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  foreignKey: 'product_id'
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+  as: 'tag_products'
 });
 module.exports = {
   Product,
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -4,15 +4,31 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // The `/api/products` endpoint
 
 // get all products
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const productData = await Product.findAll({
+      include: [{ model: Category},{ model: Tag, through: ProductTag, as: 'product_tags'}]
+    });
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
   // find all products
   // be sure to include its associated Category and Tag data
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [{ model: Category},{ model: Tag, through: ProductTag, as: 'product_tags'}]
+    })
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  }
 });
 
 // create new product
@@ -89,8 +105,20 @@ router.put('/:id', (req, res) => {
     });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {id: req.params.id}
+    });
+    if (!productData) {
+      res.status(404).json({ message: 'Product not found' });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
